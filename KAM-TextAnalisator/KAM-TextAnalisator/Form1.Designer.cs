0cbae09f87b3ee9050f0c5f015591e13a10e6e0a
Andrei <kostychik@bk.ru>
2021-06-16 23:47:24 +0300

    First part of Ex has done

---
+++ KAM-TextAnalisator/KAM-TextAnalisator/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace KAM_TextAnalisator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(31, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open for analisys";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button2.Location = new System.Drawing.Point(31, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "save in JSON from tb3";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.textBox1.Location = new System.Drawing.Point(31, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 60);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "C:\\Users\\Andrei\\Desktop\\text.txt";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textBox2.Location = new System.Drawing.Point(248, 26);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(611, 190);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(248, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(611, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Content of opened file";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(383, 222);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(476, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Output data";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.textBox3.Location = new System.Drawing.Point(383, 244);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(476, 190);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button5.Location = new System.Drawing.Point(31, 136);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(186, 80);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "To make calculating";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 222);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox4.Size = new System.Drawing.Size(354, 212);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = resources.GetString("textBox4.Text");
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(865, 4);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox5.Size = new System.Drawing.Size(327, 430);
+            this.textBox5.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1204, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ KAM-TextAnalisator/KAM-TextAnalisator/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace KAM_TextAnalisator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("." + ((int)'.').ToString());
+            MessageBox.Show("," + ((int)',').ToString());
+            MessageBox.Show(":" + ((int)':').ToString());
+            MessageBox.Show(";" + ((int)';').ToString());
+            MessageBox.Show("\"" + ((int)'\"').ToString());
+
+
+            //            textBox4.Text = "Прочитать текстовый файл (который может быть больших размеров), посчитать в нем:"+
+            //"-количество букв всего; количества каждой буквы" +
+            //"- количество цифр; количество чисел" +
+            //"- количество слов всего; количества каждого слова" +
+            //"- количество строк" +
+            //"- количество слов с дефисом" +
+            //"- количество знаков препинания"+
+            //"- самое длинное слово" +
+            //  "Сохранить вычисления в файле с именем: < имя исходного файла>.json в формате JSON.Причем результаты должны быть отсортированы по количеству в убывающем порядке.";
+        }
+
+
+        // ready
+        string fileName = "";
+
+
+
+
+        int NumberCount = 0;
+
+        int wordsCount = 0;
+        string[] wordsString = new string[26];
+        int[] words = new int[26];
+
+        int wordsHyphenCount = 0;
+
+        int longestWord = 0;
+
+
+        /// <summary>
+        /// Open needed file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            //// читаем файл в строку
+            //string fileText = System.IO.File.ReadAllText(filename);
+
+            textBox1.Text = openFileDialog1.FileName;
+            textBox2.Text = System.IO.File.ReadAllText(filename);
+
+
+            fileName = openFileDialog1.SafeFileName;
+
+        }
+
+
+
+
+        /// <summary>
+        /// Wtite into needed file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            // получаем выбранный файл
+            string filename = saveFileDialog1.FileName;
+            // сохраняем текст в файл
+            System.IO.File.WriteAllText(filename, textBox3.Text);
+
+        }
+
+
+
+
+        /// <summary>
+        ///  Calculating
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // ready
+                int linesCount = 0;
+                // ready
+                string filePath = "";
+                // ready
+                int fileSize = 0;
+                // ready
+                int numeralCount = 0;
+                // ready
+                int lettersCount = 0;
+                // ready
+                int[] lettersChar = new int[52];
+                // ready
+                int[] letters = new int[52];
+                // ready
+                int punctuationCount = 0;
+
+
+                lettersCount = 0;
+                filePath = textBox1.Text;
+                linesCount = System.IO.File.ReadAllLines(filePath).Length;
+                string[] STRING = new string[linesCount];
+                textBox2.Text = System.IO.File.ReadAllText(filePath);
+
+
+                StreamReader sr = new StreamReader(filePath);
+                {
+                    int i = 0;
+                    while (!sr.EndOfStream)
+                    {
+                        STRING[i] += sr.ReadLine();
+                        i++;
+                    }
+                }
+
+
+
+                #region  Amount of letter and numeral (English)
+                for (int i = 0; i < STRING.Length; i++)
+                {
+                    fileSize += STRING[i].Length;
+                    for (int j = 0; j < STRING[i].Length; j++)
+                    {
+                        if ((STRING[i][j] >= 97 && STRING[i][j] <= 122) || (STRING[i][j] >= 65 && STRING[i][j] <= 90))
+                        {
+                            lettersCount++;
+                        }
+                        if (STRING[i][j] >= 48 && STRING[i][j] <= 57)
+                        {
+                            numeralCount++;
+                        }
+                    }
+                }
+                #endregion
+
+
+                #region  Amount of every letter
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    if (i < 26)
+                    {
+                        lettersChar[i] = i + 97;
+                    }
+                    if (i > 26)
+                    {
+                        lettersChar[i] = i + 38;
+                    }
+
+                }
+
+
+                for (int i = 0; i < STRING.Length; i++)
+                {
+
+                    for (int j = 0; j < STRING[i].Length; j++)
+                    {
+                        for (int k = 0; k < letters.Length; k++)
+                        {
+                            //MessageBox.Show($"{STRING[i][j]} and {(char)(lettersChar[k])}");
+                            if (STRING[i][j] == (char)(lettersChar[k]))
+                            {
+                                letters[k]++;
+                            }
+                        }
+                    }
+                }
+
+                textBox5.Text = "";
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    textBox5.Text += (char)lettersChar[i] + "=" + letters[i] + "\r\n";
+
+                }
+
+                #endregion
+
+
+                #region  Amount of PUNCTUATION
+                int tt = 0;
+                for (int i = 0; i < STRING.Length; i++)
+                {
+                    for (int j = 0; j < STRING[i].Length; j++)
+                    {
+                        if (STRING[i][j] == 46 || STRING[i][j] == 44 || STRING[i][j] == 58 || STRING[i][j] == 59 || STRING[i][j] == 34)
+                        {
+                            if (STRING[i][j] == 34)
+                            {
+                                tt++;
+                            }
+                            else
+                            {
+                                punctuationCount++;
+                            }
+                        }
+                    }
+                }
+                punctuationCount +=  (tt/2);
+                #endregion
+
+
+
+
+                textBox3.Text = "";
+                textBox3.Text +=
+                     "{\r\n" +
+                      $"\"fileName\":\"{fileName}\", \r\n" +
+                      $"\"fileSize\":\"{fileSize}\", \r\n" +
+                      $"\"lettersCount\":\"{lettersCount}\", \r\n" +
+                      $"\"numeralCount\":\"{numeralCount}\", \r\n" +
+                      $"\"NumberCount\":\"{NumberCount}\", \r\n" +
+                      $"\"wordsCount\":\"{wordsCount}\", \r\n" +
+                      $"\"linesCount\":\"{linesCount}\", \r\n" +
+                      $"\"wordsHyphenCount\":\"{wordsHyphenCount}\", \r\n" +
+                      $"\"punctuationCount\":\"{punctuationCount}\", \r\n" +
+                      $"\"longestWord\":\"{longestWord}\", \r\n" +
+
+                            "words {\r\n" +
+                                 $"\"a\":\"{23}\" \r\n" +
+                                 $"\"b\":\"{25}\" \r\n" +
+                            "}, \r\n";
+                textBox3.Text += "letters { \r\n";
+
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    textBox3.Text += $"\"{(char)lettersChar[i]}\":\"{letters[i]}\", \r\n";
+                }
+                textBox3.Text += "},\r\n";
+
+                //for (int i = 0; i < words.Length; i++)
+                //{
+                //    textBox3.Text += $"\"a:\"{words[i]}";
+                //}
+                //textBox3.Text += "}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
