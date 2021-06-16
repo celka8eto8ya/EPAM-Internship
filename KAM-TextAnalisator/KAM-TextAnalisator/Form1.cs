a0e66e216019d63a9f71e051856a209637107063
Andrei <kostychik@bk.ru>
2021-06-17 01:23:22 +0300

    almost All is ready

--- KAM-TextAnalisator/KAM-TextAnalisator/Form1.cs
+++ KAM-TextAnalisator/KAM-TextAnalisator/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows.Forms;
 
@@ -9,11 +10,10 @@ namespace KAM_TextAnalisator
         public Form1()
         {
             InitializeComponent();
-            MessageBox.Show("." + ((int)'.').ToString());
-            MessageBox.Show("," + ((int)',').ToString());
-            MessageBox.Show(":" + ((int)':').ToString());
-            MessageBox.Show(";" + ((int)';').ToString());
-            MessageBox.Show("\"" + ((int)'\"').ToString());
+            MessageBox.Show("" + ((int)'0').ToString());
+            MessageBox.Show(" " + ((int)'1').ToString());
+            MessageBox.Show(" " + ((int)'9').ToString());
+
 
 
             //            textBox4.Text = "Прочитать текстовый файл (который может быть больших размеров), посчитать в нем:"+
@@ -32,17 +32,11 @@ namespace KAM_TextAnalisator
         string fileName = "";
 
 
+      
 
+        List<string> wordsName = new List<string>();
+        List<int> words = new List<int>();
 
-        int NumberCount = 0;
-
-        int wordsCount = 0;
-        string[] wordsString = new string[26];
-        int[] words = new int[26];
-
-        int wordsHyphenCount = 0;
-
-        int longestWord = 0;
 
 
         /// <summary>
@@ -71,8 +65,6 @@ namespace KAM_TextAnalisator
         }
 
 
-
-
         /// <summary>
         /// Wtite into needed file
         /// </summary>
@@ -90,8 +82,6 @@ namespace KAM_TextAnalisator
             System.IO.File.WriteAllText(filename, textBox3.Text);
 
         }
-
-
 
 
         /// <summary>
@@ -119,6 +109,17 @@ namespace KAM_TextAnalisator
                 int[] letters = new int[52];
                 // ready
                 int punctuationCount = 0;
+                // ready
+                int wordsCount = 0;
+                // ready
+                List<string> wordsString = new List<string>();
+                // ready
+                string longestWord = "";
+                // ready
+                int wordsHyphenCount = 0;
+                // ready
+                int numberCount = 0;
+
 
 
                 lettersCount = 0;
@@ -219,8 +220,148 @@ namespace KAM_TextAnalisator
                         }
                     }
                 }
-                punctuationCount +=  (tt/2);
+                punctuationCount += (tt / 2);
                 #endregion
+
+
+                #region  Amount of WORDS + largestWord + wordsHyphenCount + numberCount
+                for (int i = 0; i < STRING.Length; i++)
+                {
+
+                    STRING[i] = STRING[i].Replace("  ", " ");
+                    STRING[i] = STRING[i].Replace("   ", " ");
+                    STRING[i] = STRING[i].Replace("    ", " ");
+                    STRING[i] = STRING[i].Replace("     ", " ");
+                    STRING[i] = STRING[i].Replace("      ", " ");
+                    STRING[i] = STRING[i].Replace("       ", " ");
+                    STRING[i] = STRING[i].Replace("        ", " ");
+                    STRING[i] = STRING[i].Replace("         ", " ");
+                    STRING[i] = STRING[i].Replace("          ", " ");
+                    STRING[i] = STRING[i].Replace("           ", " ");
+                    string[] MASS = STRING[i].Split(' ');
+
+                    for (int l = 0; l < MASS.Length; l++)
+                    {
+                        int count = 0;
+                        int countNum = 0;
+                        for (int m = 0; m < MASS[l].Length; m++)
+                        {
+                            //MessageBox.Show(MASS[l]);
+
+                            if ((MASS[l][m] >= 97 && MASS[l][m] <= 122) || (MASS[l][m] >= 65 && MASS[l][m] <= 90) || MASS[l][m] == 96 || MASS[l][m] == 39 || MASS[l][m] == 45)
+                            {
+                                count++;
+                            }
+                            if (MASS[l][m] >= 48 && MASS[l][m] <= 57)
+                            {
+                                countNum++;
+                            }
+
+                        }
+                        if (count == MASS[l].Length && count != 0)
+                        {
+                            wordsString.Add(MASS[l]);
+                            wordsCount++;
+                        }
+                        if (countNum == MASS[l].Length && countNum != 0)
+                        {
+                            numberCount++;
+                        }
+
+                    }
+                }
+                MessageBox.Show(wordsString.Count.ToString());
+                textBox5.Text = "";
+                string maxWord = wordsString[0];
+                int maxWordLength = wordsString[0].Length;
+                for (int l = 0; l < wordsString.Count; l++)
+                {
+                    if (wordsString[l].IndexOf("-") > 0)
+                    {
+                        wordsHyphenCount++;
+                    }
+                    if (wordsString[l].Length > maxWordLength)
+                    {
+                        maxWordLength = wordsString[l].Length;
+                        longestWord = wordsString[l];
+
+                    }
+                    textBox5.Text += $"\"{wordsString[l]}\"\r\n";
+
+
+                    //// WORDS
+                    //words.Add(1);
+                    //wordsName.Add(wordsString[l]);
+                    ////MessageBox.Show(words.Count.ToString());
+                    ////MessageBox.Show(wordsName.Count.ToString());
+                    //int COUNT = words.Count;
+                    ////for (int i = 0; i < words.Count; i++)
+                    //while (COUNT>0)
+                    //{
+                    //    int i = 0;
+                    //    if (wordsString[l] == wordsName[i] && l != 0)
+                    //    {
+                    //        words[i]++;
+                    //    }
+                    //    else
+                    //    {
+                    //        words.Add(1);
+                    //        wordsName.Add(wordsString[l]);
+                    //    }
+                    //    i++;
+                    //}
+
+                    //textBox5.Text += $"\r\n";
+                    //textBox5.Text += $"\r\n";
+                    //for (int i = 0; i < words.Count; i++)
+                    //{
+                    //    textBox5.Text += $"\"{wordsName[i]}\":\"{words[i]}\", \r\n";
+                    //}
+                }
+
+                MessageBox.Show(longestWord);
+
+                #endregion
+
+
+                #region  Amount of Number 
+                for (int i = 0; i < STRING.Length; i++)
+                {
+
+                    STRING[i] = STRING[i].Replace("  ", " ");
+                    STRING[i] = STRING[i].Replace("   ", " ");
+                    STRING[i] = STRING[i].Replace("    ", " ");
+                    STRING[i] = STRING[i].Replace("     ", " ");
+                    STRING[i] = STRING[i].Replace("      ", " ");
+                    STRING[i] = STRING[i].Replace("       ", " ");
+                    STRING[i] = STRING[i].Replace("        ", " ");
+                    STRING[i] = STRING[i].Replace("         ", " ");
+                    STRING[i] = STRING[i].Replace("          ", " ");
+                    STRING[i] = STRING[i].Replace("           ", " ");
+                    string[] MASS = STRING[i].Split(' ');
+
+                    for (int l = 0; l < MASS.Length; l++)
+                    {
+                        int count = 0;
+                        for (int m = 0; m < MASS[l].Length; m++)
+                        {
+                            //MessageBox.Show(MASS[l]);
+
+                            if ((MASS[l][m] >= 97 && MASS[l][m] <= 122) || (MASS[l][m] >= 65 && MASS[l][m] <= 90) || MASS[l][m] == 96 || MASS[l][m] == 39 || MASS[l][m] == 45)
+                            {
+                                count++;
+                            }
+
+                        }
+                        if (count == MASS[l].Length && count != 0)
+                        {
+                            wordsString.Add(MASS[l]);
+                            wordsCount++;
+                        }
+                    }
+                }
+                #endregion
+
 
 
 
@@ -232,7 +373,7 @@ namespace KAM_TextAnalisator
                       $"\"fileSize\":\"{fileSize}\", \r\n" +
                       $"\"lettersCount\":\"{lettersCount}\", \r\n" +
                       $"\"numeralCount\":\"{numeralCount}\", \r\n" +
-                      $"\"NumberCount\":\"{NumberCount}\", \r\n" +
+                      $"\"numberCount\":\"{numberCount}\", \r\n" +
                       $"\"wordsCount\":\"{wordsCount}\", \r\n" +
                       $"\"linesCount\":\"{linesCount}\", \r\n" +
                       $"\"wordsHyphenCount\":\"{wordsHyphenCount}\", \r\n" +
